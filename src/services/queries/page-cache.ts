b6461c33363d64b8a16705bc6ad8cfd7d97ae760
Odafe Idogho <poidogho@yahoo.com>
2022-12-13 22:05:04 -0500

    first redis client setup

--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,19 @@
-export const getCachedPage = (route: string) => {};
+import { client } from "$services/redis";
+import { pageCacheKey } from "$services/keys";
 
-export const setCachedPage = (route: string, page: string) => {};
+const cachedRoutes = new Set<string>([
+    '/about', '/privace','/auth/signin','/auth/signout'
+])
+
+export const getCachedPage = (route: string) => {
+
+    return cachedRoutes.has(route) ? client.get(pageCacheKey(route)) : null
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if(cachedRoutes.has(route)){
+        return client.set(pageCacheKey(route), page, {
+            EX: 2
+        })
+    }
+};
